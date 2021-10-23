e9d67416b38d64aacd1fdef257e6236a2394e4ab
terza98 <terza98@hotmail.rs>
2021-10-23 23:07:09 +0200

    add login screen

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import AppButton from "../components/AppButton";
+import AppTextInput from "../components/AppTextInput";
+import { Formik } from "formik";
+
+import Screen from "../components/Screen";
+
+function LoginScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <>
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              onChangeText={handleChange("email")}
+              placeholder="Email"
+              textContentType="emailAddress"
+            />
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              placeholder="Password"
+              onChangeText={handleChange("password")}
+              secureTextEntry
+              textContentType="password"
+            />
+            <AppButton title="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+export default LoginScreen;
