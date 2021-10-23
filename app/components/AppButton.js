d5da23f615c4a076efceccf9db8b041f711bf14c
terza98 <terza98@hotmail.rs>
2021-10-23 23:06:51 +0200

    rename prop

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -2,13 +2,13 @@ import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import colors from "../config/colors";
 
-function AppButton({ text, onPress, color = "primary" }) {
+function AppButton({ title, onPress, color = "primary" }) {
   return (
     <TouchableOpacity
       style={[styles.button, { backgroundColor: colors[color] }]}
       onPress={onPress}
     >
-      <Text style={styles.text}>{text}</Text>
+      <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
 }
