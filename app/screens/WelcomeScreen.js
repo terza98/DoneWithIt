860e9a087ea12c970b6160e064daa33c3caef0a6
terza98 <terza98@hotmail.rs>
2021-10-23 10:37:04 +0200

    add welcome screen skeleton

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+
+      <AppButton color="primary">Login</AppButton>
+      <AppButton color="secondary">Register</AppButton>
+    </ImageBackground>
+  );
+}
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+});
+
+export default WelcomeScreen;
