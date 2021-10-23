cb8e1a0ddb756e6eea992296b25a4b07de374e96
terza98 <terza98@hotmail.rs>
2021-10-23 17:07:15 +0200

    refactor

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -2,7 +2,7 @@ import React from "react";
 import Constants from "expo-constants";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 
-function Screen({ children, style }) {
+function Screen({ children, style = {} }) {
   return (
     <SafeAreaView style={[styles.screen, style]}>
       <View style={style}>{children}</View>
