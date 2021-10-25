c102e0df8a6b38394ca3d34e827b094bc8e88c63
terza98 <terza98@hotmail.rs>
2021-10-25 10:15:30 +0200

    fix component issue

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import AnimatedLottieView from "lottie-react-native";
 
 function ActivityIndicator({ visible = false }) {
+  const animation = useRef();
+  useEffect(() => {
+    animation.current.play();
+  }, []);
+
   if (!visible) return null;
   return (
     <AnimatedLottieView
+      ref={animation}
       source={require("../assets/animations/loader.json")}
       autoplay
       loop
