4008aa4d59d3cceffa52cdc5fe5491582f54ea90
terza98 <terza98@hotmail.rs>
2021-11-07 13:36:22 +0100

    refactor useAPI for error handling

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -2,24 +2,18 @@ import { useState } from "react";
 
 export default useApi = (apiFunc) => {
   const [data, setData] = useState([]);
-  const [error, setError] = useState();
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const request = async ({ ...args }) => {
+  const request = async (...args) => {
     setLoading(true);
-    const response = await apiFunc({ ...args });
+    const response = await apiFunc(...args);
     setLoading(false);
 
-    if (!response.ok) return setError(true);
-
-    setError(false);
+    setError(!response.ok);
     setData(response.data);
+    return response;
   };
 
-  return {
-    data,
-    error,
-    loading,
-    request,
-  };
+  return { data, error, loading, request };
 };
